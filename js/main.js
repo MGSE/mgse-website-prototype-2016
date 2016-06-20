b2abb9e7e875b2b4764170c677b521f6bd1bc673
jporich <jporich@gmail.com>
2016-06-20 13:09:24 +1000

    MGSE events sorting
    Made some slight changes to events page to use same sorting list,
    
    Added in “#container .mix” selector in query so same effect in news
    sorting is duplicated across for event listings.

--- js/main.js
+++ js/main.js
@@ -134,11 +134,11 @@ $(document).ready(function () {
 	function sortItems(currentItem) {
 		if(currentItem == "all") {
 			
-			$(".news-wrap").each(function() {
+			$(".news-wrap, #container .mix").each(function() {
 			    $(this).slideDown("medium");
 			});
 		} else {
-			$(".news-wrap").each(function() {
+			$(".news-wrap, #container .mix").each(function() {
 				if($(this).hasClass(currentItem))
 				{
 					$(this).slideDown("medium"); //Show
